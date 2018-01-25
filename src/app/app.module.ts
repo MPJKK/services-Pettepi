c03c3099c03e6feca4740017f49b3ecae1cfaff7
Petteri Piironen <pettepi@ety-a2123b-14.edu.wks.metropolia.fi>
2018-01-25 13:12:54 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListMediaComponent } from './list-media/list-media.component';
+import {MediaService} from './services/media.service';
+import {HttpClientModule} from '@angular/common/http';
+import {DigitransitService} from './services/digitransit.service';
+import { RoutesComponent } from './routes/routes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListMediaComponent,
+    RoutesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [MediaService, DigitransitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routes/routes.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {DigitransitService} from '../services/digitransit.service';
+
+@Component({
+    selector: 'app-routes',
+    templateUrl: './routes.component.html',
+    styleUrls: ['./routes.component.css']
+})
+export class RoutesComponent implements OnInit {
+
+    pysakki = 'Gransinmäki';
+
+    constructor(private digitransitService: DigitransitService) {
+    }
+
+    ngOnInit() {
+        this.digitransitService.getRoutes(this.pysakki).subscribe(response => {
+            console.log(response.data['stops']);
+        });
+
+    }
+
+}
